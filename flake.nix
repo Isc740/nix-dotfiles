4f4c18e79bf190a7a92550152cd99bd909a0a516
Isc740 <isaazcantillo@gmail.com>
2025-06-02 10:55:49 -0500

    add fjordlauncher

--- flake.nix
+++ flake.nix
@@ -8,40 +8,51 @@
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     nvf.url = "github:notashelf/nvf";
+
+    fjordlauncher.url = "github:unmojang/FjordLauncher";
   };
 
-  outputs = { self, nixpkgs, home-manager, nvf, ... }@inputs:
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs {
-        inherit system;
-        config = { allowUnfree = true; };
-      };
-    in {
-
-      packages.x86_64-linux.my-neovim = (nvf.lib.neovimConfiguration {
+  outputs = {
+    self,
+    nixpkgs,
+    home-manager,
+    nvf,
+    fjordlauncher,
+    ...
+  } @ inputs: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+      config = {allowUnfree = true;};
+    };
+  in {
+    packages.x86_64-linux.my-neovim =
+      (nvf.lib.neovimConfiguration {
         pkgs = nixpkgs.legacyPackages.x86_64-linux;
-        modules = [ ./nvf/nvf-module.nix ];
+        modules = [./nvf/nvf-module.nix];
       }).neovim;
 
-      nixosConfigurations = {
-        nixos = nixpkgs.lib.nixosSystem {
-          specialArgs = { inherit system; };
+    nixosConfigurations = {
+      nixos = nixpkgs.lib.nixosSystem {
+        specialArgs = {inherit system;};
 
-          modules = [
-            ./nixos/configuration.nix
-            home-manager.nixosModules.home-manager
-            {
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
+        modules = [
+          ./nixos/configuration.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
 
-              home-manager.users.isc740 = import ./home-manager/home.nix;
-            }
-            # ({pkgs, ...}: {
-            #   environment.systemPackages = [self.packages.${pkgs.stdenv.system}.my-neovim];
-            # })
-          ];
-        };
+            home-manager.users.isc740 = import ./home-manager/home.nix;
+          }
+          ({pkgs, ...}: {
+            environment.systemPackages = [
+              # self.packages.${pkgs.stdenv.system}.my-neovim
+              # fjordlauncher.packages.${pkgs.system}.fjordlauncher
+            ];
+          })
+        ];
       };
     };
+  };
 }
