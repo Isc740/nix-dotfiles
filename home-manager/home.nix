5f9ab695daa62217c1ea889fd3fa0c7d2e1567ef
Isc740 <isaazcantillo@gmail.com>
2025-06-15 21:57:18 -0500

    add nix-shell-zsh to zsh

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -58,6 +58,18 @@
         nixupdate = "sudo nix flake update ~/nix/";
         ll = "ls -la";
       };
+      plugins = [
+        {
+          name = "zsh-nix-shell";
+          file = "nix-shell.plugin.zsh";
+          src = pkgs.fetchFromGitHub {
+            owner = "chisui";
+            repo = "zsh-nix-shell";
+            rev = "v0.8.0";
+            sha256 = "1lzrn0n4fxfcgg65v0qhnj7wnybybqzs4adz7xsrkgmcsr0ii8b7";
+          };
+        }
+      ];
       oh-my-zsh = {
         enable = true;
         plugins = [
